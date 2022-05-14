5a46aa2645611e3a8ed106a6fe41965fefa2d650
Vitalia Svirskaja <vitalia.svirskaja.vs@gmail.com>
2022-05-14 22:11:06 +0200

    Functions for creating api requests

--- src/api.ts
+++ src/api.ts
@@ -1,31 +1,46 @@
-import { Weather } from "./weather-interface";
 import { apiKey } from "./weather-api-key";
+import { WeatherApiForecastData } from "./weather-api-forecast-data";
 import { WeatherApiData } from "./weather-api-data";
-import { mapWeatherData } from "./weather-mapping";
-const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+import { Position } from "./position";
 
-export async function getWeather(city: string): Promise<Weather> {
-  const url = new URL(BASE_URL);
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
+
+async function getPositionByCityName(city: string): Promise<Position> {
+  const url = new URL(BASE_URL + "/weather");
   url.searchParams.append("q", city);
   url.searchParams.append("APPID", apiKey);
   url.searchParams.append("units", "metric");
   url.searchParams.append("lang", "de");
-  let response: Response = await fetch(url.toString(), {
+  let weatherByCityResponse: Response = await fetch(url.toString(), {
     mode: "cors",
   });
-  let weatherData: WeatherApiData = await response.json();
-  return mapWeatherData(weatherData);
+  const weatherApiData: WeatherApiData = await weatherByCityResponse.json();
+  return {
+    longitude: weatherApiData.coord.lon,
+    latitude: weatherApiData.coord.lat,
+  };
 }
 
-export async function getWeatherByLongLat(longitude: number, latitude: number) {
-  const url = new URL(BASE_URL);
+export async function getWeatherByCity(
+  city: string
+): Promise<WeatherApiForecastData> {
+  const { latitude, longitude } = await getPositionByCityName(city);
+  return getWeatherByLongLat(longitude, latitude);
+}
+
+export async function getWeatherByLongLat(
+  longitude: number,
+  latitude: number
+): Promise<WeatherApiForecastData> {
+  const url = new URL(BASE_URL + "/onecall");
   url.searchParams.append("lat", latitude.toString());
   url.searchParams.append("lon", longitude.toString());
   url.searchParams.append("appid", apiKey);
   url.searchParams.append("lang", "de");
-  let response: Response = await fetch(url.toString(), {
+  url.searchParams.append("units", "metric");
+  url.searchParams.append("exclude", "minutely, alerts");
+  const response: Response = await fetch(url.toString(), {
     mode: "cors",
   });
-  let weatherData: WeatherApiData = await response.json();
-  return mapWeatherData(weatherData);
+  return response.json();
 }
